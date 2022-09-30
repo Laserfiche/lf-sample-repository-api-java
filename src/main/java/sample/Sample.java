8b8e70959b3bf12201a7d94ff308a4fe24ec06cb
Steven Xu <steven.xu@laserfiche.com>
2022-09-30 11:55:17 -0400

    resolved comments

--- src/main/java/sample/Sample.java
+++ src/main/java/sample/Sample.java
@@ -11,29 +11,30 @@ import java.util.concurrent.CompletableFuture;
 
 public class Sample {
     private static final int ROOTFOLDERENTRYID = 1;
-    private static RepositoryApiClient client = createRepoApiClient();
+    private static RepositoryApiClient client;
 
     public static void main(String[] args) {
-        String repoName = getRepoName().join();
+        client = createRepositoryApiClient();
+        String repositoryName = getRepositoryName().join();
         Entry rootFolder = getRootFolder().join();
         List<Entry> rootFolderChildren = getFolderChildren(ROOTFOLDERENTRYID).join();
         System.exit(0);
     }
 
-    public static CompletableFuture<String> getRepoName() {
-        CompletableFuture<List<RepositoryInfo>> repoListResponse = client.getRepositoryClient().getRepositoryList();
-        CompletableFuture<String> repoName = repoListResponse.supplyAsync(() ->
-                repoListResponse.join().get(0).getRepoName());
-        CompletableFuture<String> repoId = repoListResponse.supplyAsync(() ->
-                repoListResponse.join().get(0).getRepoId());
-        repoId.thenAcceptAsync(repodIdAsync -> {
-            System.out.println("Repository Name: " + repodIdAsync);
+    public static CompletableFuture<String> getRepositoryName() {
+        CompletableFuture<List<RepositoryInfo>> repositoryListResponse = client.getRepositoryClient().getRepositoryList();
+        CompletableFuture<String> repositoryName = repositoryListResponse.supplyAsync(() ->
+                repositoryListResponse.join().get(0).getRepoName());
+        CompletableFuture<String> repositoryId = repositoryListResponse.supplyAsync(() ->
+                repositoryListResponse.join().get(0).getRepoId());
+        repositoryId.thenAcceptAsync(repositoryIdAsync -> {
+            System.out.println("Repository Name: " + repositoryIdAsync);
         });
-        return repoName;
+        return repositoryName;
     }
 
     public static CompletableFuture<Entry> getRootFolder() {
-        CompletableFuture<Entry> entryResponse = client.getEntriesClient().getEntry(ServiceConfig.repoId, ROOTFOLDERENTRYID, null);
+        CompletableFuture<Entry> entryResponse = client.getEntriesClient().getEntry(ServiceConfig.repositoryId, ROOTFOLDERENTRYID, null);
         CompletableFuture<Entry> entry = entryResponse.supplyAsync(() ->
                 entryResponse.join());
         CompletableFuture<String> rootFolderName = entry.supplyAsync(() -> entry.join().getName());
@@ -47,7 +48,7 @@ public class Sample {
     }
 
     public static CompletableFuture<List<Entry>> getFolderChildren(int folderEntryId) {
-        CompletableFuture<ODataValueContextOfIListOfEntry> folderChildren = client.getEntriesClient().getEntryListing(ServiceConfig.repoId, folderEntryId, true, null, null, null, null, null, "name", null, null, null, null);
+        CompletableFuture<ODataValueContextOfIListOfEntry> folderChildren = client.getEntriesClient().getEntryListing(ServiceConfig.repositoryId, folderEntryId, true, null, null, null, null, null, "name", null, null, null, null);
         CompletableFuture<List<Entry>> children = folderChildren.supplyAsync(() -> folderChildren.join().getValue());
         children.thenAcceptAsync(childrenAsync -> {
             int i = 0;
@@ -58,8 +59,8 @@ public class Sample {
         return children;
     }
 
-    public static RepositoryApiClient createRepoApiClient() {
+    public static RepositoryApiClient createRepositoryApiClient() {
         ServiceConfig.setUp();
-        return RepositoryApiClientImpl.CreateFromAccessKey(ServiceConfig.spKey, ServiceConfig.accessKey);
+        return RepositoryApiClientImpl.CreateFromAccessKey(ServiceConfig.servicePrincipalKey, ServiceConfig.accessKey);
     }
 }
--- src/main/java/sample/ServiceConfig.java
+++ src/main/java/sample/ServiceConfig.java
@@ -4,19 +4,19 @@ import com.laserfiche.api.client.model.AccessKey;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class ServiceConfig {
-    protected static String spKey;
+    protected static String servicePrincipalKey;
     protected static AccessKey accessKey;
-    protected static String repoId;
+    protected static String repositoryId;
 
     private ServiceConfig() {
         throw new IllegalStateException("Utility class");
     }
 
     public static void setUp() {
-        spKey = System.getenv("SERVICE_PRINCIPAL_KEY");
+        servicePrincipalKey = System.getenv("SERVICE_PRINCIPAL_KEY");
         String accessKeyBase64 = System.getenv("ACCESS_KEY");
-        repoId = System.getenv("REPOSITORY_ID");
-        if (spKey == null && accessKeyBase64 == null && repoId == null) {
+        repositoryId = System.getenv("REPOSITORY_ID");
+        if (servicePrincipalKey == null && accessKeyBase64 == null && repositoryId == null) {
             // Load environment variables
             Dotenv dotenv = Dotenv
                     .configure()
@@ -24,8 +24,8 @@ public class ServiceConfig {
                     .load();
             // Read env variable
             accessKeyBase64 = dotenv.get("ACCESS_KEY");
-            repoId = dotenv.get("REPOSITORY_ID");
-            spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+            repositoryId = dotenv.get("REPOSITORY_ID");
+            servicePrincipalKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
         }
         accessKey = AccessKey.CreateFromBase64EncodedAccessKey(accessKeyBase64);
     }
