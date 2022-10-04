0a5337fcf22955135c1186de4b589c56eb4577b5
Steven Xu <steven.xu@laserfiche.com>
2022-10-04 12:00:19 -0400

    resolved comments and refactored

--- src/main/java/sample/Sample.java
+++ src/main/java/sample/Sample.java
@@ -20,33 +20,33 @@ public class Sample {
 
     public static CompletableFuture<String> getRepositoryName() {
         CompletableFuture<List<RepositoryInfo>> repositoryListResponse = client.getRepositoryClient().getRepositoryList();
-        CompletableFuture<String> repositoryName = repositoryListResponse.thenApplyAsync(repositoryInfos -> repositoryInfos.get(0).getRepoName());
-        CompletableFuture<String> repositoryId = repositoryListResponse.thenApplyAsync(repositoryInfoList -> repositoryInfoList.get(0).getRepoId());
-        repositoryId.thenAcceptAsync(repositoryIdAsync -> {
-            System.out.println("Repository Name: " + repositoryIdAsync);
+        CompletableFuture<String> repositoryName = repositoryListResponse.thenApply(repositoryInfos -> repositoryInfos.get(0).getRepoName());
+        CompletableFuture<String> repositoryId = repositoryListResponse.thenApply(repositoryInfoList -> repositoryInfoList.get(0).getRepoId());
+        repositoryId.thenAccept(repositoryIdResult -> {
+            System.out.println("Repository Name: " + repositoryIdResult);
         });
         return repositoryName;
     }
 
     public static CompletableFuture<Entry> getRootFolder() {
         CompletableFuture<Entry> entryResponse = client.getEntriesClient().getEntry(ServiceConfig.repositoryId, ROOTFOLDERENTRYID, null);
-        CompletableFuture<Entry> entry = entryResponse.thenApplyAsync(entryResponseInfo -> entryResponseInfo);
-        CompletableFuture<String> rootFolderName = entry.thenApplyAsync(entryName -> entryName.getName());
-        rootFolderName.thenAcceptAsync(rootFolderNameAsync -> {
-            if (rootFolderNameAsync.length() == 0) {
-                rootFolderNameAsync = "/";
+        CompletableFuture<Entry> entry = entryResponse.thenApply(entryResponseInfo -> entryResponseInfo);
+        CompletableFuture<String> rootFolderName = entry.thenApply(entryName -> entryName.getName());
+        rootFolderName.thenAccept(rootFolderNameResult -> {
+            if (rootFolderNameResult.length() == 0) {
+                rootFolderNameResult = "/";
             }
-            System.out.println("Root Folder Name: " + rootFolderNameAsync);
+            System.out.println("Root Folder Name: " + rootFolderNameResult);
         });
         return entry;
     }
 
     public static CompletableFuture<List<Entry>> getFolderChildren(int folderEntryId) {
         CompletableFuture<ODataValueContextOfIListOfEntry> folderChildren = client.getEntriesClient().getEntryListing(ServiceConfig.repositoryId, folderEntryId, true, null, null, null, null, null, "name", null, null, null, null);
-        CompletableFuture<List<Entry>> children = folderChildren.thenApplyAsync(folderChildrenInfo -> folderChildrenInfo.getValue());
-        children.thenAcceptAsync(childrenAsync -> {
+        CompletableFuture<List<Entry>> children = folderChildren.thenApply(folderChildrenInfo -> folderChildrenInfo.getValue());
+        children.thenAccept(childrenResult -> {
             int i = 0;
-            for (Entry child : childrenAsync) {
+            for (Entry child : childrenResult) {
                 System.out.printf("%s:[%s id:%d] '%s'%n", i++, child.getEntryType(), child.getId(), child.getName());
             }
         });
