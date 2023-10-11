21e149620980fcfdfbed98814aff7f69b5028ec8
Samad Paydar <samad.paydar@laserfiche.com>
2023-10-11 10:51:18 -0400

    Optimize import statements.

--- src/main/java/sample/Sample.java
+++ src/main/java/sample/Sample.java
@@ -10,7 +10,9 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Sample {
